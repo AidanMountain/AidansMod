37913e0e07a626a7147108ee3fff3a9e7ba35468
AidanMountain <mountaina20@gmail.com>
2024-03-18 16:03:19 -0400

    Cycle 3

---
+++ src/main/java/net/aidanmountain/aidansmod24/mixin/BlockMixin.java
@@ -0,0 +1,20 @@
+package net.aidanmountain.aidansmod24.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.level.block.Block;
+
+
+@Mixin(Block.class)
+public class BlockMixin {
+  @Inject(at = @At(value = "HEAD"), method = "Lnet/minecraft/world/level/block/Block;getFriction()F", cancellable = true)
+  protected float getFriction(CallbackInfo info){
+    info.cancel(); 
+
+    return 0.02f;
+  }
+}
